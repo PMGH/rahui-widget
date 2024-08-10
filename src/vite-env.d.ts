1860839dd675c47328c328eec7fede737648502f
PMGH <petermccready2@gmail.com>
2024-08-10 17:56:40 +0100

    add types for env variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_IS_DEVELOPMENT: string;
+  readonly VITE_TEST_ROOT_ELEMENT: string;
+  readonly VITE_RAHUI_BOOKING_LOCAL_SERVER_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
