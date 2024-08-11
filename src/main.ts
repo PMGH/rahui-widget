05129cbcd2e555745b5bcdc1dd9f822b8db3596c
PMGH <petermccready2@gmail.com>
2024-08-11 13:45:43 +0100

    use import.meta.env.mode

--- src/main.ts
+++ src/main.ts
@@ -413,10 +413,7 @@ class RahuiWidget {
 }
 
 // The build is using injected env vars from .env so we need to use VITE_IS_PRODUCTION to mean we don't need to reset the .env vars each time we do a build
-if (
-  !import.meta.env.VITE_IS_PRODUCTION &&
-  import.meta.env.VITE_IS_DEVELOPMENT === "true"
-) {
+if (import.meta.env.MODE === "development") {
   const testRootElementId = "test-root-element";
   const isRootElementTest = import.meta.env.VITE_TEST_ROOT_ELEMENT === "true";
   if (isRootElementTest) {
